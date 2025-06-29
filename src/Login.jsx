42aec40d02f71d6d0d9b290089c2a1a4ab630611
scksbb <sckv@outlook.com>
2025-06-29 20:55:24 +0800

    8

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,41 +1,37 @@
-import {Form, Input, Button} from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { Form, Card, Input, Button } from "antd";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/");
+  };
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Card className="w-350px  text-center bg-white shadow-lg">
+        <h1 className="mb-6">Login</h1>
+        <Form name="login" initialValues={{ remember: true }} layout="vertical">
+          <Form.Item label="Username" name="username">
+            <Input />
+          </Form.Item>
 
-const navigate = useNavigate();
-const handleLogin = () => {
-    navigate('/');
-};
-return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-        <h1 className='mb-6'>Login </h1>
-        <Form
-            name="login"
-            style={{ maxWidth: 300 }}
-            initialValues={{ remember: true }}
-        >
-            <Form.Item
-                label="Username"
-                name="username"
+          <Form.Item label="Password" name="password">
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="button"
+              block
+              onClick={handleLogin}
             >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Password"
-                name="password"
-            >
-                <Input.Password />
-            </Form.Item>
-
-            <Form.Item>
-                <Button type="primary" htmlType="button" block onClick={handleLogin}>
-                    Login
-                </Button>
-            </Form.Item>
+              Login
+            </Button>
+          </Form.Item>
         </Form>
+      </Card>
     </div>
-);
+  );
 };
 
 export default Login;
